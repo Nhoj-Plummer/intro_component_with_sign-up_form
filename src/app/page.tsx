408806913f18eff3a8651c4a09dab6bcd0612e37
Nhoj-Plummer <nhojplummer@gmail.com>
2025-03-11 10:41:04 -0400

    Setting up for the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import SignUp from "@/app/sign-up/SignUp";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <SignUp />
+    </>
+  );
 }
---
+++ src/app/sign-up/SignUp.tsx
@@ -0,0 +1,11 @@
+import "@/app/sign-up/SignUp.css";
+
+const SignUp = () => {
+  return (
+    <div>
+      <p>Sign Up section</p>
+    </div>
+  );
+};
+
+export default SignUp;
