0ef02556d1ebf0aa4e25064b988a4c075beab900
Nhoj Plummer <nhojp@example.com>
2025-04-18 11:06:01 -0400

    Setting up base css foundation

--- src/app/sign-up/SignUp.tsx
+++ src/app/sign-up/SignUp.tsx
@@ -2,8 +2,15 @@ import "@/app/sign-up/SignUp.css";
 
 const SignUp = () => {
   return (
-    <div>
-      <p>Sign Up section</p>
+    <div className="sign_up_container">
+      <div className="sign_up_text_container">
+        <h1>Learn to code by watching others</h1>
+        <p>See how experienced developers solve problems in real-time. Watcing scripted tutorials is great, but understanding how developers think is invaluable.</p>
+      </div>
+      <div className="sign_up_inputs_container">
+        <div className="sign_up_inputs_reminder">Try it free 7 days then $20/mo. thereafter</div>
+        <div className="sign_up_inputs_fields"></div>
+      </div>
     </div>
   );
 };
