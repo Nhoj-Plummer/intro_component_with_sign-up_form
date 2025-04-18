0d3c269227a96408eca858fd2a8041eb54e25860
Nhoj Plummer <nhojp@example.com>
2025-04-18 18:17:13 -0400

    Setting up base iput field code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+"use client"
+import React, {useState} from "react";
 import SignUp from "@/app/sign-up/SignUp";
 import InputField from "@/components/input-field/input-field";
 
+
 export default function Home() {
+const [firstName, setFirstName] = useState("");
+
   return (
     <>
       <SignUp />
-      <InputField />
+      <InputField  
+        placeholder="First Name" 
+        type="text" 
+        errorMessage="First Name cannot be empty" 
+        onChange={(value) => setFirstName(value)}/>
     </>
   );
 }
--- src/components/input-field/input-field.tsx
+++ src/components/input-field/input-field.tsx
@@ -1,21 +1,52 @@
-import React from "react";
+'use client'
+import React, {useState} from "react";
 import "./input-field.css";
 
 interface InputFieldProps {
-    input?: string
     placeholder?: string
     type?: string
+    errorMessage?: string;
+    onChange?: (value: string) => void;
 }
 
-const InputField: React.FC<InputFieldProps> = ({input, placeholder, type}) =>{
+const InputField: React.FC<InputFieldProps> = ({
+    placeholder="", 
+    type="text", 
+    errorMessage,
+    onChange
+}) => {
+
+    const [value, setValue] = useState('');
+    const [error, setError] = useState<string | null>(null);
+
+    const validateInput = (value: string) => {
+        if (type === "text" && value.trim() === "") {
+            return `${errorMessage || 'This field'} cannot be empty`;
+    } 
+    return null}
+
+    const checkInput=(e: React.ChangeEvent<HTMLInputElement>) =>{
+        const newValue = e.target.value
+        setValue(newValue)
+
+        if (onChange) {
+            onChange(newValue);
+        }
+
+        const validationError = validateInput(newValue);
+        setError(validationError);
+    }
     
     return (
-        <><input 
-        id="input-field"
-        type={type}
-        value={input}
-        placeholder={placeholder}
-        className="input_field"/></>
+        <div className="input_field_container">
+            <input 
+                id="input-field"
+                type={type}
+                value={value}
+                placeholder={placeholder}
+                className={`input_field ${error ? "input_error" :""}`}
+                onChange={checkInput}/>
+        </div>
     )
 }
 
