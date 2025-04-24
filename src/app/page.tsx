f4eb9ce6ced9c32f38783f10aed58bd6517370cf
Nhoj Plummer <nhojp@example.com>
2025-04-24 12:32:26 -0400

    Ading general format to button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import React from "react";
 import SignUp from "@/app/sign-up/SignUp";
+import Button from "@/components/button/button";
 
 export default function Home() {
   return (
     <>
       <SignUp />
+      <Button text="Testing Field!" />
     </>
   );
 }
