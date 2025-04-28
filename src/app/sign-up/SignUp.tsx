09ebd4f1ed6e95fece6c150c960fe107cfcf401f
Nhoj Plummer <nhojp@example.com>
2025-04-28 11:53:14 -0400

    Adding formatting

--- src/app/sign-up/SignUp.tsx
+++ src/app/sign-up/SignUp.tsx
@@ -7,8 +7,30 @@ const SignUp = () => {
   const [firstName, setFirstName] = useState("");
 
   return (
-    <div>
-      <p>Sign Up section</p>
+    <div className="sign_up_container">
+      <div className="sign_up_text_container">
+        <h1>Learn to code by watching others</h1>
+        <p>
+          See how experienced developers solve problems in real-time. Watcing
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
+        </p>
+      </div>
+      <div className="sign_up_inputs_container">
+        <div className="sign_up_inputs_reminder">
+          Try it free 7 days then $20/mo. thereafter
+        </div>
+        <div className="sign_up_inputs_fields">
+          <InputField
+            placeholder="First Name"
+            type="text"
+            errorMessage="First Name "
+            onChange={(value) => setFirstName(value)}
+            errorPlaceholder="email@example/com"
+          />
+        </div>
+        <Button text="CLAIM YOUR FREE TRIAL" />
+      </div>
     </div>
   );
 };
